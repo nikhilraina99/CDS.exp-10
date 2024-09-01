707fc0c47abb15e9d4e69366d0d1e2914778858f
Nikhil <rainanikhilr@gmail.com>
2024-09-02 01:07:43 +0530

    Add files via upload

---
+++ exp10a_callbyreference.cpp
@@ -0,0 +1,19 @@
+// Nikhil
+// 23070123093
+// experiment 10
+#include <iostream>
+using namespace std;
+void swap(int *a,int* b)
+{
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+  cout<<"Inside swapByValue- "<<"a = "<< *a<<" , "<<"b = "<< *b<<endl;
+}
+ int main()
+{ int a=5,b=10;
+cout<<"Before swapByValue- "<<"a = "<<a<<" , "<<"b = "<<b<<endl;
+swap(&a,&b); 
+cout<<"After swapByValue- "<<"a = "<<a<<" , "<<"b = "<<b<<endl;
+}
